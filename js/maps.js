8c8ffedcfde3599e94f1b4e2c956d0744c6bfe3b
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2022-02-13 12:54:01 +0100

    Make map work again

--- js/maps.js
+++ js/maps.js
@@ -7,28 +7,35 @@ $(document).ready(function () {
 });
 
 function draw_today_map() {
-  var today_map = L.map('today-map', {
+  var today_map = L.map("today-map", {
     center: [63.3985, 10.3933],
     scrollWheelZoom: false,
     maxZoom: 18,
-    zoom: 11
+    zoom: 11,
   });
 
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    accessToken: 'pk.eyJ1IjoiaGFuc2hlbnJpayIsImEiOiJjaWo0aTAzdncwMDIwdXJrcm5jZDZ6dXJ4In0.00I3RVVHNDAvi0XnbZEU-Q',
-    id: 'mapbox.streets' // Or one of these: https://www.mapbox.com/developers/api/maps/#tiles
-  }).addTo(today_map);
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      accessToken:
+        "pk.eyJ1IjoiaGFuc2hlbnJpayIsImEiOiJjaWo0aTAzdncwMDIwdXJrcm5jZDZ6dXJ4In0.00I3RVVHNDAvi0XnbZEU-Q",
+      tileSize: 512,
+      zoomOffset: -1,
+      id: "mapbox/streets-v11", // Or one of these: https://www.mapbox.com/developers/api/maps/#tiles
+    }
+  ).addTo(today_map);
 
   var options = {
-    color: 'black',
-    fillOpacity: .5,
-    opacity: 1
-  }
-  L.circleMarker([63.357514, 10.371765], options).addTo(today_map)
-  L.circleMarker([63.417531, 10.396615], options).addTo(today_map)
-  L.circleMarker([63.430473, 10.393609], options).addTo(today_map)
-  L.circleMarker([63.432923, 10.410652], options).addTo(today_map)
+    color: "black",
+    fillOpacity: 0.5,
+    opacity: 1,
+  };
+  L.circleMarker([63.357514, 10.371765], options).addTo(today_map);
+  L.circleMarker([63.417531, 10.396615], options).addTo(today_map);
+  L.circleMarker([63.430473, 10.393609], options).addTo(today_map);
+  L.circleMarker([63.432923, 10.410652], options).addTo(today_map);
 
   // tiller =       63.357514, 10.371765
   // elgeseter =    63.417531, 10.396615
@@ -39,50 +46,61 @@ function draw_today_map() {
 }
 
 function draw_future_map() {
-  var future_map = L.map('future-map', {
+  var future_map = L.map("future-map", {
     center: [63.4235, 10.3933],
     scrollWheelZoom: false,
     maxZoom: 18,
-    zoom: 13
+    zoom: 13,
   });
 
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    accessToken: 'pk.eyJ1IjoiaGFuc2hlbnJpayIsImEiOiJjaWo0aTAzdncwMDIwdXJrcm5jZDZ6dXJ4In0.00I3RVVHNDAvi0XnbZEU-Q',
-    id: 'mapbox.streets'
-  }).addTo(future_map);
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      accessToken:
+        "pk.eyJ1IjoiaGFuc2hlbnJpayIsImEiOiJjaWo0aTAzdncwMDIwdXJrcm5jZDZ6dXJ4In0.00I3RVVHNDAvi0XnbZEU-Q",
+      tileSize: 512,
+      zoomOffset: -1,
+      id: "mapbox/streets-v11",
+    }
+  ).addTo(future_map);
 
   // Generate 200 randomly placed markers
   for (i = 0; i < 200; i++) {
-    var lat = 63.436 -(Math.random()*0.03)
-    var lon = 10.33 + (Math.random()*0.15)
+    var lat = 63.436 - Math.random() * 0.03;
+    var lon = 10.33 + Math.random() * 0.15;
     var value = Math.floor(Math.random() * 700) + 300;
     var color = "blue";
     if (value > 950) color = "red";
     if (value < 650) color = "green";
 
-    L.circleMarker([lat, lon],
-      {
-        color: color
-      })
+    L.circleMarker([lat, lon], {
+      color: color,
+    })
       .addTo(future_map)
-      .bindPopup("<b>Node X</b><br><b>Latest CO2 level</b>: "+value+"<br><b>Time</b>: 2016-01-07T17:04:42.453Z")
-      .on('dblclick', onMarkerDblClick);
+      .bindPopup(
+        "<b>Node X</b><br><b>Latest CO2 level</b>: " +
+          value +
+          "<br><b>Time</b>: 2016-01-07T17:04:42.453Z"
+      )
+      .on("dblclick", onMarkerDblClick);
   }
 
   var popup = L.popup();
   function onMapClick(e) {
     popup
       .setLatLng(e.latlng)
-      .setContent("<a href='#'>Request drone measurement</a> at this location<br><a href='#'>Request permanent sensor</a> at this location") //+ e.latlng.toString())
+      .setContent(
+        "<a href='#'>Request drone measurement</a> at this location<br><a href='#'>Request permanent sensor</a> at this location"
+      ) //+ e.latlng.toString())
       .openOn(future_map);
   }
   function onMarkerDblClick(e) {
-    alert('hi')
+    alert("hi");
   }
 
-  future_map.on('click', onMapClick);
-
+  future_map.on("click", onMapClick);
 
   // var polygon = L.polygon([
   //   [63.4297, 10.3930],
@@ -90,5 +108,5 @@ function draw_future_map() {
   //   [63.4242, 10.3822]
   // ]).addTo(map);
 
-return future_map;
+  return future_map;
 }
